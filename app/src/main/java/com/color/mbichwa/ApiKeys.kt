40a56b211fb400c0e3b8086711b8e7e674651586
jaysonamati <jaysonamati@gmail.com>
2020-07-09 13:47:24 +0300

    Added Location functions in the checkout page

---
+++ app/src/main/java/com/color/mbichwa/ApiKeys.kt
@@ -0,0 +1,4 @@
+package com.color.mbichwa
+
+class ApiKeys(val places_api_key:String = "AIzaSyASFzc47dzjo5h0HuFyx0ZR4u2mHRQR93A") {
+}
--- app/src/main/java/com/color/mbichwa/pages/home/CheckoutFragment.kt
+++ app/src/main/java/com/color/mbichwa/pages/home/CheckoutFragment.kt
@@ -1,7 +1,11 @@
 package com.color.mbichwa.pages.home
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,15 +13,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import com.color.mbichwa.ApiKeys
 import com.color.mbichwa.R
 import com.color.mbichwa.databinding.FragmentCheckoutBinding
 import com.color.mbichwa.pages.home.models.OrderedProduct
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import timber.log.Timber
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,8 +49,11 @@ class CheckoutFragment : Fragment() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val locationRequestCode = 1000
-    private val wayLatitude = 0.0
-    private val wayLongitude = 0.0
+    private var wayLatitude = 0.0
+    private var wayLongitude = 0.0
+    private val REQUEST_CHECK_SETTINGS = 1
+    private lateinit var currentLocation:Location
+    private val AUTOCOMPLETE_REQUEST_CODE = 1
 
 
     private lateinit var binding: FragmentCheckoutBinding
@@ -89,10 +105,55 @@ class CheckoutFragment : Fragment() {
             )
 
         }
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
+
+        fun createLocationRequest(){
+            val locationRequest = LocationRequest.create()?.apply {
+                interval  = 1000
+                fastestInterval = 5000
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+
+            val builder = locationRequest?.let {
+                LocationSettingsRequest.Builder()
+                    .addLocationRequest(it)
+            }
+
+            val client: SettingsClient? = context?.let { LocationServices.getSettingsClient(it) }
+            val task: Task<LocationSettingsResponse> = client!!.checkLocationSettings(builder!!.build())
+
+            task.addOnSuccessListener { locationSettingsResponse ->
 
             }
+
+            task.addOnFailureListener { exception ->
+                if (exception is ResolvableApiException){
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+//                Toast.makeText(context,"Change your location Settings",Toast.LENGTH_SHORT).show()
+                    try {
+                        exception.startResolutionForResult(activity,REQUEST_CHECK_SETTINGS)
+                    } catch (sendEx:IntentSender.SendIntentException){
+                        // Ignore the error
+                    }
+                }
+            }
+        }
+        createLocationRequest()
+        context?.let { Places.initialize(it,ApiKeys().places_api_key) }
+        val placesClient = context?.let { Places.createClient(it) }
+
+        val fields = listOf(Place.Field.ID, Place.Field.LAT_LNG,Place.Field.NAME, Place.Field.ADDRESS)
+
+        val intent =
+            context?.let {
+                Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
+                    .build(it)
+            }
+
+
+
+
+
 //        checkPermissionForLocation()
 
         cartItems = ArrayList()
@@ -111,8 +172,52 @@ class CheckoutFragment : Fragment() {
 
         })
 
+        binding.paymentOptionsRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.mpesaRadioButton -> {
+                    binding.mpesaDetailsConstraintLayout.visibility = View.VISIBLE
+                    binding.cardDetailsConstraintLayout.visibility = View.GONE
+                }
+                R.id.cardRadioButton -> {
+                    binding.mpesaDetailsConstraintLayout.visibility = View.GONE
+                    binding.cardDetailsConstraintLayout.visibility = View.VISIBLE
+                }
+            }
+        }
+        binding.currentLocationSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked){
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        if (location != null){
+                            currentLocation = location
+                            val geocoder:Geocoder = Geocoder(context)
+                            val addresses = geocoder.getFromLocation(location.latitude,location.longitude,5)
+                            if (addresses.size > 0){
+                                val address  = addresses[0]
+                                binding.selectedLocationNameTextView.text = address.featureName
+                            }
+                            wayLatitude = location.latitude
+                            wayLongitude = location.longitude
+                        } else {
+                            binding.currentLocationSwitch.isChecked = false
+                        }
+                    }
+            } else if (!isChecked){
+                binding.selectedLocationNameTextView.text = getString(R.string.location_not_set_text)
+            }
+        }
+
+        binding.searchLocationEditText.setOnClickListener {
+            binding.currentLocationSwitch.isChecked = false
+            startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
+        }
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onRadioButtonClicked(view)
     }
 
     fun onRadioButtonClicked(view:View){
@@ -122,14 +227,38 @@ class CheckoutFragment : Fragment() {
             when(view.id){
                 R.id.mpesaRadioButton ->
                     if (checked){
-
+                        Toast.makeText(context,"Mpesa",Toast.LENGTH_SHORT).show()
                     }
                 R.id.cardRadioButton ->
                     if (checked){
-
+                        binding.cardDetailsConstraintLayout.visibility = View.VISIBLE
                     }
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE){
+            when(resultCode){
+                Activity.RESULT_OK -> {
+                    data?.let {
+                        val place = Autocomplete.getPlaceFromIntent(data)
+                        binding.selectedLocationNameTextView.text = place.name
+                    }
+                }
+                AutocompleteActivity.RESULT_ERROR ->{
+                    data?.let {
+                        val status = Autocomplete.getStatusFromIntent(data)
+                        Timber.e(status.statusMessage)
+                }
+            }
+                Activity.RESULT_CANCELED -> {
+                    // User has cancelled the operation
+                }
+            }
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun checkPermissionForLocation() {
@@ -168,10 +297,18 @@ class CheckoutFragment : Fragment() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
+            locationRequestCode ->
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(context, "Permission granted", Toast.LENGTH_SHORT).show();
 
+                }else{
+                    Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
 
         }
     }
+
+
 
     companion object {
         /**
